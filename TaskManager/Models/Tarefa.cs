8d710e6e415df7b8dc4e51aad677723fdc011519
Sandyla Stocler <sanystocler@gmail.com>
2025-06-23 15:00:16 -0300

    inglês tradutor para português

---
+++ TaskManager/Models/Tarefa.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace taskManager.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataConclusao { get; set; }
+    }
+}        
